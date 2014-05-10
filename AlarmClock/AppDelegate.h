69fdc132d0a9cf570fbff39ac874d5fd278d5125
Angel Vazquez <angelvazquez@Mac-de-Angel.local>
2014-05-10 22:21:48 +0200

    Initial Commit

---
+++ AlarmClock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlarmClock
+//
+//  Created by Angel Vazquez on 10/05/14.
+//  Copyright (c) 2014 uax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlarmClock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlarmClock
+//
+//  Created by Angel Vazquez on 10/05/14.
+//  Copyright (c) 2014 uax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
